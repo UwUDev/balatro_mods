624762ad5a9179ef0966c92c26af296d8decc0ec
UwU <mmimi0593@gmail.com>
2024-03-01 13:04:24 +0100

    Test index

--- sample_mod/mods/sample_mod.lua
+++ sample_mod/mods/sample_mod.lua
@@ -2,7 +2,7 @@ table.insert(mods,
         {
             mod_id = "sample_mod",
             name = "Sample Mod",
-            enabled = false,
+            enabled = true,
             on_pre_render = function()
                 love.graphics.print("Sample mod loaded", 500, 80)
             end
