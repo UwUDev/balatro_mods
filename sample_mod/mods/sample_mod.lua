399c7e3893d950ad175a9a9e0053152177223f3e
UwU <mmimi0593@gmail.com>
2024-03-01 13:11:07 +0100

    Test mod

--- sample_mod/mods/sample_mod.lua
+++ sample_mod/mods/sample_mod.lua
@@ -3,8 +3,8 @@ table.insert(mods,
             mod_id = "sample_mod",
             name = "Sample Mod",
             enabled = true,
-            on_pre_render = function()
-                love.graphics.print("Sample mod loaded", 500, 80)
+            on_post_render = function()
+                love.graphics.print("Sample mod loaded", 500, 30)
             end
         }
 )
